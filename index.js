b21f3f324a21546251542e11e5f7aae518ce4d50
fedek1324 <fedua1324@yandex.ru>
2023-04-13 23:38:07 +0300

    add empty link alert

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function showDropdown() {
+function showDropdown(e) {
     console.log('set dropdown');
     document.getElementsByClassName("welcome_dropdown-content")[0].classList.toggle("l-flex");
+    console.log(e)
 }
+
+function emptyLink() {
+    alert('Это ссылка пуста!')
+}
+
+$( "a" ).click(function() {
+    alert( "Перехватили событие" );
+  });
